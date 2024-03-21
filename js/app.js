ad1e47c4891b159da0fe7dc2b8c3e87e4f8a78fe
Vaggelis kavouras <vaggeliskavouras6@gmail.com>
2024-03-21 19:10:09 +0200

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var App = function(){
-	this.model = new Model();
-	this.model.init();
-	this.view  = new View(this.model);
-	this.view.init();
-	// this.controller = new Controller(this.model, this.view);
-
-	// this.controller.init();
-};
--- js/util.js
+++
@@ -1,4 +0,0 @@
-var dateToddMonFormat = function(date){
-	let options = { month : "short", day : "2-digit", year : "numeric"};
-	return date.toLocaleDateString("en-IN", options);
-};
